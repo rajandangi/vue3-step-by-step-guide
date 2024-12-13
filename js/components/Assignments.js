80ba74db90d18ac556b636e4cb52b73e4ed9345a
rajandangi <rajandangi649@gmail.com>
2024-12-13 16:34:07 +1030

    Event Handling : Handle form submission

--- js/components/Assignments.js
+++ js/components/Assignments.js
@@ -7,6 +7,13 @@ export default {
         <section class="space-y-6">
             <assignment-list :assignments="filters.inProgress" title="In Progress"></assignment-list>
             <assignment-list :assignments="filters.completed" title="Completed"></assignment-list>
+
+            <form @submit.prevent="add">
+                <div class="border border-gray-600 text-black">
+                    <input type="text" placeholder="New Assignment" class="p-2" v-model="newAssignment" />
+                    <button type="submit" class="bg-white p-2 border-l">Add</button>
+                </div>
+            </form>
         </section>
     `,
 
@@ -17,6 +24,8 @@ export default {
                 { name: "Read chapter 4", completed: false, id: 2 },
                 { name: "Turn in homework", completed: false, id: 3 },
             ],
+
+            newAssignment: "",
         };
     },
 
@@ -30,4 +39,16 @@ export default {
             }
         }
     },
+
+    methods: {
+        add() {
+            this.assignments.push({
+                name: this.newAssignment,
+                completed: false,
+                id: this.assignments.length + 1,
+            })
+
+            this.newAssignment = "";
+        }
+    }
 };
