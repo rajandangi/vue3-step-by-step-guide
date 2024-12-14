531f36dffa5e8f07f096b4a1a5583ba5a0e4a84e
rajandangi <rajandangi649@gmail.com>
2024-12-14 13:23:29 +1030

    Add AssignmentTags component for tag filtering in AssignmentList

--- js/components/AssignmentList.js
+++ js/components/AssignmentList.js
@@ -1,7 +1,8 @@
 import Assignment from "./Assignment.js";
+import AssignmentTags from "./AssignmentTags.js";
 
 export default {
-    components: { Assignment },
+    components: { Assignment, AssignmentTags },
 
     template: `
         <section v-show="assignments.length">
@@ -10,15 +11,11 @@ export default {
                     <span>({{ assignments.length }})</span>
                  </h2>
 
-                 <div class="flex gap-2">
-                    <button
-                    @click="currentTag=tag"
-                    v-for="tag in tags"
-                    :key="tag"
-                    class="border rounded px-1 py-px text-xs"
-                    :class="{ 'border-blue-500 text-blue-500': tag===currentTag}"
-                    >{{tag}}</button>
-                 </div>
+                <assignment-tags
+                    :initial-tags="assignments.map(a =>a.tag)"
+                    @change="currentTag = $event"
+                    :current-tag="currentTag"
+                    ></assignment-tags>
 
                 <ul class="border border-gray-600 divide-y divide-gray-600 mt-6">
                     <assignment
@@ -49,11 +46,5 @@ export default {
 
             return this.assignments.filter(a => a.tag === this.currentTag);
         },
-
-        tags() {
-            // Map transform each element in the array and returns a new array
-            // Set removes duplicates
-            return ['all', ...new Set(this.assignments.map(a => a.tag))];
-        }
     }
 }
