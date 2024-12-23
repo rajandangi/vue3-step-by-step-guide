eef329d4cee4dfe7113a8243496bfb7899211f5d
rajandangi <rajandangi649@gmail.com>
2024-12-23 15:06:34 +1030

    Build a Modal component

---
+++ src/components/Teams/TeamModal.vue
@@ -0,0 +1,63 @@
+<script setup>
+defineProps({
+  show: Boolean,
+});
+</script>
+
+<template>
+  <Transition
+    enter-from-class="opacity-0 scale-125"
+    enter-to-class="opacity-100 scale-100"
+    enter-active-class="transition duration-300"
+    leave-active-class="transition duration-200"
+    leave-from-class="opacity-100 scale-100"
+    leave-to-class="opacity-0 scale-125"
+  >
+    <div class="modal-mask" v-if="show">
+      <div class="modal-container">
+        <div>
+          <slot>This is default body</slot>
+        </div>
+        <footer class="modal-footer">
+          <slot name="footer">
+            <button @click="$emit('closeModal')">Close Modal</button>
+          </slot>
+        </footer>
+      </div>
+    </div>
+  </Transition>
+</template>
+
+<style>
+.modal-mask {
+  position: fixed;
+  inset: 0;
+  background: rgba(0, 0, 0, 0.6);
+  display: grid;
+  place-items: center;
+}
+.modal-container {
+  background: white;
+  padding: 1rem;
+  border-radius: 7px;
+  width: 80vw;
+  max-width: 500px;
+}
+
+.modal-footer {
+  border-top: 1px solid #ddd;
+  margin-top: 1rem;
+  padding-top: 0.5rem;
+  font-size: 0.8rem;
+}
+
+.modal-footer button {
+  background: #ddd;
+  padding: 0.25rem 0.75rem;
+  border-radius: 1rem;
+}
+
+.modal-footer button:hover {
+  background: #c8c8c8;
+}
+</style>
