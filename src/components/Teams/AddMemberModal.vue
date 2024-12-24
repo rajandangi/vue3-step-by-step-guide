dd8c7dc10ba5030759c600130ed381b103abdbec
rajandangi <rajandangi649@gmail.com>
2024-12-24 11:36:20 +1030

    Teleport in vuejs

---
+++ src/components/Teams/AddMemberModal.vue
@@ -0,0 +1,30 @@
+<script setup>
+import Modal from "@/components/Teams/TeamModal.vue";
+import {useTeamStore} from "@/stores/TeamStore.js";
+import { ref } from "vue";
+
+let showModal = ref(false);
+let team = useTeamStore();
+</script>
+
+<template>
+  <button
+    class="bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded disabled:bg-gray-400"
+    :disabled="!team.spotsRemaining"
+    @click="showModal = true"
+  >
+    Add Member ({{ team.spotsRemaining }} Spots Left)
+  </button>
+
+  <Teleport to="body">
+    <Modal :show="showModal" @close-modal="showModal = false">
+      <template #default>
+        <p>Need to add a new member to your team?</p>
+        <div class="flex mt-6 gap-3">
+          <input type="email" class="flex-1" />
+          <button>Add</button>
+        </div>
+      </template>
+    </Modal>
+  </Teleport>
+</template>
--- src/views/TeamView.vue
+++ src/views/TeamView.vue
@@ -3,35 +3,18 @@ import { useTeamStore } from "@/stores/TeamStore.js";
 
 let team = useTeamStore();
 team.fill();
-// setTimeout(() => {
-//   team.grow(25);
-// }, 2000);
 
 import TeamHeader from "@/components/Teams/TeamHeader.vue";
 import TeamMembers from "@/components/Teams/TeamMembers.vue";
 import TeamFooter from "@/components/Teams/TeamFooter.vue";
-import Modal from "@/components/Teams/TeamModal.vue";
-import { ref } from "vue";
-
-let showModal = ref(false);
 </script>
 
 <template>
-  <TeamHeader @open-modal="showModal = true" />
+  <TeamHeader />
 
   <div class="place-self-center flex flex-col gap-y-3">
     <TeamMembers />
   </div>
 
   <TeamFooter />
-
-  <Modal :show="showModal" @close-modal="showModal = false">
-    <template #default>
-      <p>Need to add a new member to your team?</p>
-      <div class="flex mt-6 gap-3">
-        <input type="email" class="flex-1" />
-        <button>Add</button>
-      </div>
-    </template>
-  </Modal>
 </template>
